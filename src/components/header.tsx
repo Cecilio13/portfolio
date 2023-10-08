f18f0c48c42eab777b18fa993a938abdce3df9a7
Cecilio13 <ceciliodeticio13@gmail.com>
2023-10-08 08:40:44 +0800

    .

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,18 @@
-
-interface HeaderProps {
-  title : string
-}
-const Header = ({title}:HeaderProps) => {
-  return (
-    <h1>
-      {title}
-    </h1>
-  );
+const Header = ({}) => {
+  return [
+    <div className="section section-header">
+      <div className="header-container">
+        <div className="header-img"></div>
+        <div className="header-txt-container">
+          <div className="header-intro">Hi, I'm</div>
+          <div className="header-name">Cecilio Frederico D. Deticio</div>
+          <div className="header-profession">Web Developer</div>
+          <div className="btn-container">
+            <button className="btn btn-csv">Download CSV</button>
+          </div>
+        </div>
+      </div>
+    </div>,
+  ];
 };
-
 export default Header;
